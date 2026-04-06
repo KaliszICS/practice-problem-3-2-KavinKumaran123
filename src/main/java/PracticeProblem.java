24336ce5cd6ed73d472d4b1e7d9c12acbe1cff4e
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-04-06 21:44:40 +0000

    3.2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,16 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
 
+	public static int towerOfHanoi(int num) {
+    	if (num < 3)
+        	return -1;
+
+    	if (num == 3)
+        	return 7;
+
+    	return 2 * towerOfHanoi(num - 1) + 1;
 	}
 
 }
+
+
